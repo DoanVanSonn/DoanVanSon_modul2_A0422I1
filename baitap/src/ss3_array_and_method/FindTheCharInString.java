05f29e71940371d4dfe55121adbaa8e2ac1ce7c2
DoanVanSonn <sonmonter123@gmai.com>
2022-07-15 18:08:51 +0700

    bonus

--- baitap/src/ss3_array_and_method/FindTheCharInString.java
+++ baitap/src/ss3_array_and_method/FindTheCharInString.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 
 public class FindTheCharInString {
     public static void main(String[] args) {
-        String str="a,f,e,r,h,y,u";
+        String[] str=" Doan Van Son".split("");
         Scanner scanner = new Scanner(System.in);
         System.out.println("nhập vào kí tự cần tìm kiếm :");
-        String c = scanner.nextLine();
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(Integer.parseInt(c))!=-1){
-
-            }
+        char c = scanner.next().charAt(0);
+        int count=0;
+        for (int i = 0; i < str.length; i++) {
+if (str[i].charAt(0)==c){
+    count++;
+}
         }
+        System.out.println("so lan lap lai la "+ count);
 
 
     }
